239d6433aa2cedae0b963cd41d8f51fd099f8d4b
RICHARD <richardmatgomes@gmail.com>
2026-05-29 11:27:04 -0300

    adaptação ao notebook

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db_parametros = {
     "usuario" : 'postgres',
-    "senha" : 'nainha04131120@',
+    "senha" : 'admin',
     "host":'localhost',
     "porta": '5432',
     "bd" : 'rvlgeoloc'
 }
 db = SQLAlchemy()
-db_url = f'postgresql://{db_parametros["usuario"]}:{db_parametros["senha"]}{db_parametros["host"]}:{db_parametros["porta"]}/{db_parametros["bd"]}'
+db_url = f'postgresql://{db_parametros["usuario"]}:{db_parametros["senha"]}@{db_parametros["host"]}:{db_parametros["porta"]}/{db_parametros["bd"]}'
 db_nome = {"projeto" : "rvlgeoloc", "litoestatigrafia" : "banco_lito"}
 engine = create_engine(db_url)
 query_lito = "select * from public.banco_lito where lower(litotipo2) like '%%carbonatito%%' or lower(litotipo1) like '%% piroxenito%%'"
